de7c4a6608c09ce075ab217335f2fe738dfc0e86
Nekit K. <14nikita@list.ru>
2026-05-16 18:51:13 +0500

    Academy start

---
+++ Academy/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Academy
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.tabPageGroups = new System.Windows.Forms.TabPage();
+			this.tabPageStudents = new System.Windows.Forms.TabPage();
+			this.tabControl = new System.Windows.Forms.TabControl();
+			this.tabPageDirections = new System.Windows.Forms.TabPage();
+			this.tabPageDiscipline = new System.Windows.Forms.TabPage();
+			this.tabPageTeacher = new System.Windows.Forms.TabPage();
+			this.tabControl.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.Location = new System.Drawing.Point(0, 296);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Size = new System.Drawing.Size(687, 22);
+			this.statusStrip.TabIndex = 0;
+			this.statusStrip.Text = "statusStrip1";
+			// 
+			// tabPageGroups
+			// 
+			this.tabPageGroups.Location = new System.Drawing.Point(4, 22);
+			this.tabPageGroups.Name = "tabPageGroups";
+			this.tabPageGroups.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageGroups.Size = new System.Drawing.Size(679, 270);
+			this.tabPageGroups.TabIndex = 1;
+			this.tabPageGroups.Text = "Groups";
+			this.tabPageGroups.UseVisualStyleBackColor = true;
+			// 
+			// tabPageStudents
+			// 
+			this.tabPageStudents.Location = new System.Drawing.Point(4, 22);
+			this.tabPageStudents.Name = "tabPageStudents";
+			this.tabPageStudents.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageStudents.Size = new System.Drawing.Size(679, 270);
+			this.tabPageStudents.TabIndex = 0;
+			this.tabPageStudents.Text = "Students";
+			this.tabPageStudents.UseVisualStyleBackColor = true;
+			// 
+			// tabControl
+			// 
+			this.tabControl.Controls.Add(this.tabPageStudents);
+			this.tabControl.Controls.Add(this.tabPageGroups);
+			this.tabControl.Controls.Add(this.tabPageDirections);
+			this.tabControl.Controls.Add(this.tabPageDiscipline);
+			this.tabControl.Controls.Add(this.tabPageTeacher);
+			this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl.Location = new System.Drawing.Point(0, 0);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.Size = new System.Drawing.Size(687, 296);
+			this.tabControl.TabIndex = 1;
+			// 
+			// tabPageDirections
+			// 
+			this.tabPageDirections.Location = new System.Drawing.Point(4, 22);
+			this.tabPageDirections.Name = "tabPageDirections";
+			this.tabPageDirections.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageDirections.Size = new System.Drawing.Size(679, 270);
+			this.tabPageDirections.TabIndex = 2;
+			this.tabPageDirections.Text = "Directions";
+			this.tabPageDirections.UseVisualStyleBackColor = true;
+			// 
+			// tabPageDiscipline
+			// 
+			this.tabPageDiscipline.Location = new System.Drawing.Point(4, 22);
+			this.tabPageDiscipline.Name = "tabPageDiscipline";
+			this.tabPageDiscipline.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageDiscipline.Size = new System.Drawing.Size(679, 270);
+			this.tabPageDiscipline.TabIndex = 3;
+			this.tabPageDiscipline.Text = "Discipline";
+			this.tabPageDiscipline.UseVisualStyleBackColor = true;
+			// 
+			// tabPageTeacher
+			// 
+			this.tabPageTeacher.Location = new System.Drawing.Point(4, 22);
+			this.tabPageTeacher.Name = "tabPageTeacher";
+			this.tabPageTeacher.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageTeacher.Size = new System.Drawing.Size(679, 270);
+			this.tabPageTeacher.TabIndex = 4;
+			this.tabPageTeacher.Text = "Teacher";
+			this.tabPageTeacher.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(687, 318);
+			this.Controls.Add(this.tabControl);
+			this.Controls.Add(this.statusStrip);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Atomic Acadamy  P_421";
+			this.tabControl.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.StatusStrip statusStrip;
+		private System.Windows.Forms.TabPage tabPageGroups;
+		private System.Windows.Forms.TabPage tabPageStudents;
+		private System.Windows.Forms.TabControl tabControl;
+		private System.Windows.Forms.TabPage tabPageDirections;
+		private System.Windows.Forms.TabPage tabPageDiscipline;
+		private System.Windows.Forms.TabPage tabPageTeacher;
+	}
+}
+
