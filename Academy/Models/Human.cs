ec2205acc158a05e58b60230584aaa052ccb318f
Nekit K. <14nikita@list.ru>
2026-06-06 16:39:44 +0500

    Image filters DONE

--- Academy/Models/Human.cs
+++ Academy/Models/Human.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.IO;
+using System.Security.AccessControl;
 namespace Academy.Models
 {
 	internal class Human
---
+++ SQLQuery111.sql
@@ -0,0 +1,2 @@
+﻿UPDATE Students
+SET last_name = N'Собко_',first_name = N'Анастасия',middle_name = N'Максимовна',birth_date = N'2009-10-20',email = N'Собко_',phone = N'123456789',[group]=421 WHERE stud_id= 38
